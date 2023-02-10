227465dd2f0786f721c10fe45e98cec07fe64cd2
ElleMass <leah.massey@hotmail.co.uk>
2023-02-10 09:40:34 +0000

    print all students using an until loop

---
+++ exercises/exercise_2.rb
@@ -0,0 +1,49 @@
+# Print all students using while or until control flow methods (Loops).
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create and empty array
+  students = []
+  #get the first name
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do 
+    #add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #get another name from the user
+    name = gets.chomp
+  end
+  #return the array of students 
+  students
+end
+    
+def print_header 
+  puts "The students of Villain's Academy"
+  puts "-------------"
+end 
+
+def print(students)
+    
+string_length = students.length
+accumulator = 0
+
+  until accumulator == string_length
+     puts " #{students[accumulator][:name]} ( #{students[accumulator][:cohort]} cohort)"
+    accumulator += 1
+  end
+  
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+#nothing happens until we call these methods
+students = input_students
+print_header 
+print(students)
+print_footer(students)
+
+
